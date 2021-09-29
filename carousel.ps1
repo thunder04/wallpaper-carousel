b2667dcdfe4a1601a2f7b7f003d68df22a094607
thunder04 <70254851+thunder04@users.noreply.github.com>
2021-09-29 17:13:07 +0300

    refactor: Make WallpaperFolder mandatory and set its position to 0

--- carousel.ps1
+++ carousel.ps1
@@ -1,25 +1,26 @@
 param (
-    [string] $WallpaperFolder,
-    [array] $Subreddits = ("Amoledbackgrounds", "wallpapers", "wallpaper"),
+    [Parameter(Position = 0, Mandatory = $true)]
+    [string]   $WallpaperFolder,
+
+    [array]    $Subreddits = ("Amoledbackgrounds", "wallpapers", "wallpaper"),
     [TimeSpan] $Interval = (New-TimeSpan -Hours 1),
-    [bool] $UpdateSchedule = $true,
-    [string] $Timeframe = 'all',
-    [string] $Listing = 'hot'
+    [switch]   $DontUpdateSchedule,
+    [string]   $Timeframe = 'all',
+    [string]   $Listing = 'hot'
 )
 
 $ListingTypes = ('controversial', 'best', 'hot', 'new', 'random', 'rising', 'top')
 $TimeframeTypes = ('hour', 'day', 'week', 'month', 'year', 'all')
 $ValidFileTypes = ('jpg', 'jpeg', 'webp', 'png', 'bmp')
 
-if ( -not $WallpaperFolder ) { throw 'Parameter WallpaperFolder is required' }
-if ( -not $ListingTypes.Contains($Listing)) { throw "Unknown option $Listing for Listing. Available listing types: $($ListingTypes -join ', ')" }
 if ( -not $TimeframeTypes.Contains($Timeframe)) { throw "Unknown option $Timeframe for Timeframe. Available timeframe types: $($TimeframeTypes -join ', ')" }
+if ( -not $ListingTypes.Contains($Listing)) { throw "Unknown option $Listing for Listing. Available listing types: $($ListingTypes -join ', ')" }
 
-if ($UpdateSchedule) {
+if (-not $DontUpdateSchedule) {
     if (Get-ScheduledTask WallpaperCarousel -ErrorAction Ignore) {
         Unregister-ScheduledTask WallpaperCarousel -Confirm:$false
     }
-    
+
     $action = New-ScheduledTaskAction "pwsh.exe" -Argument (
         (
             "-WindowStyle Hidden",
@@ -27,21 +28,22 @@ if ($UpdateSchedule) {
             "-Subreddits $($Subreddits -join ',')",
             "-WallpaperFolder $WallpaperFolder",
             "-Timeframe $Timeframe",
-            "-Listing $Listing",
-            "-UpdateSchedule 0"
+            "-DontUpdateSchedule",
+            "-Listing $Listing"
         ) -join ' '
     )
-            
-    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval $Interval
-    $settings = New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -Priority 6
-            
-    Register-ScheduledTask WallpaperCarousel -Trigger $trigger -Action $action -Settings $settings | Out-Null
-    Write-Host "Updated the Task Schedule."
+
+    Register-ScheduledTask WallpaperCarousel `
+        -Trigger (New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval $Interval) `
+        -Settings (New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -Priority 6) `
+        -Action $action -Force | Out-Null
+
+    Write-Host "Added Task Schedule. Remove it by running `"Unregister-ScheduledTask WallpaperCarousel -Confirm:`$false`""
     exit
 }
 
 $SubredditData = Invoke-WebRequest -Uri "https://www.reddit.com/r/$($Subreddits | Get-Random).json?listing=$Listing&t=$Timeframe&limit=5"
-$SubredditPosts = (ConvertFrom-Json $SubredditData.content).data.children | ForEach-Object { $_.data } | Where-Object { 
+$SubredditPosts = (ConvertFrom-Json $SubredditData.content).data.children | ForEach-Object { $_.data } | Where-Object {
     -not $_.banned_by -and -not $_.removed_by -and -not $_.over_18 `
         -and -not $_.is_video -and -not $_.spoiler -and $_.url `
         -and $ValidFileTypes.Contains($_.url.Split('.')[-1])
@@ -57,7 +59,7 @@ foreach ($post in $SubredditPosts) {
         Write-Debug "Found $Filename in $WallpaperFolder, skipping..."
         Continue
     }
-    
+
     try { 
         Invoke-WebRequest $post.url -OutFile $WallpaperFolder\$Filename
         Write-Debug "Downloaded $wikWallpaperFolder/$filename"
