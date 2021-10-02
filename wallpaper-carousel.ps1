201227b78d420f3736a54c3be07a4b23afc62bf3
thunder04 <70254851+thunder04@users.noreply.github.com>
2021-10-02 22:56:29 +0300

    chore: More renaming

--- wallpaper-carousel.ps1
+++ wallpaper-carousel.ps1
@@ -1,12 +1,8 @@
 param (
     [Parameter(Position = 0, Mandatory = $true)]
-    [string]   $WFolder,
-    [switch]   $NoSDL,
+    [string]   $Dir,
 
-    [TimeSpan] $Interval = (New-TimeSpan -Days 1),
-    [string]   $Timeframe = 'week',
-    [string]   $Listing = 'top',
-    [int]      $FetchLimit = 10,
+    [Parameter(Position = 1, Mandatory = $false)]
     [array]    $Subs = (
         "Amoledbackgrounds",
         "VillagePorn",
@@ -18,22 +14,34 @@ param (
         "CityPorn",
         "LakePorn",
         "SkyPorn"
-    )
+    ),
+
+    [Parameter(Position = 2, Mandatory = $false)]
+    [int]      $Mode = 0, # 0: Schedule, 1: Fetch, 2: Sweep
+
+    [TimeSpan] $Interval = (New-TimeSpan -Days 1),
+    [string]   $Timeframe = 'week',
+    [string]   $Listing = 'top',
+    [int]      $Limit = 10
 )
-
-$ListingTypes = ('controversial', 'best', 'hot', 'new', 'random', 'rising', 'top')
-$ResPattern = '[[({]\s*(?<Width>\d+)\s*x\s*(?<Height>\d+)\s*[\])}]'
-$TimeframeTypes = ('hour', 'day', 'week', 'month', 'year', 'all')
-$FileTypes = ('jpg', 'jpeg', 'webp', 'png', 'bmp')
-$ProgressPreference = "SilentlyContinue"
-
-if ( -not $TimeframeTypes.Contains($Timeframe)) { throw "Unknown option $Timeframe for Timeframe. Available timeframe types: $($TimeframeTypes -join ', ')" }
-if ( -not $ListingTypes.Contains($Listing)) { throw "Unknown option $Listing for Listing. Available listing types: $($ListingTypes -join ', ')" }
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+$ProgressPreference = "SilentlyContinue"
+
+$ListingOptions = ('controversial', 'best', 'hot', 'new', 'random', 'rising', 'top')
+$TimeframeOptions = ('hour', 'day', 'week', 'month', 'year', 'all')
+
+if ( -not $TimeframeOptions.Contains($Timeframe)) { throw "Unknown option $Timeframe for Timeframe. Available timeframe types: $($TimeframeOptions -join ', ')" }
+if ( -not $ListingOptions.Contains($Listing)) { throw "Unknown option $Listing for Listing. Available listing types: $($ListingOptions -join ', ')" }
+
+$ResPattern = '[[({]\s*(?<Width>\d+)\s*x\s*(?<Height>\d+)\s*[\])}]'
 $ScreenRes = ([System.Windows.Forms.Screen]::PrimaryScreen).Bounds
+$FileTypes = ('jpg', 'jpeg', 'webp', 'png', 'bmp')
+#$SweepInterval = (New-TimeSpan -Days 2)
+#$FileAge = (New-TimeSpan -Days 4)
+
 $MinH = if ($ScreenRes.Height -gt 720) { $ScreenRes.Height - ($ScreenRes.Height * 0.15) } else { $ScreenRes.Height }
 $MinW = if ($ScreenRes.Width -gt 1280) { $ScreenRes.Width - ($ScreenRes.Width * 0.10) } else { $ScreenRes.Width }
 
@@ -47,56 +55,60 @@ function ValidateImage(
     return $true
 }
 
-if (-not $NoSDL) {
+if ($Mode -eq 0) {
     if (Get-ScheduledTask WallpaperCarousel -ErrorAction Ignore) {
         Unregister-ScheduledTask WallpaperCarousel -Confirm:$false
     }
 
-    $Arg = (
+    $Arguments = (
         "-WindowStyle Hidden",
         "-Command .\$(Split-Path $PSCommandPath -Leaf)",
-        $WFolder,
-        "-NoSDL",
-        "-Listing $Listing",
+
+        $Dir, ($Subs -join ','), 1,
         "-Timeframe $Timeframe",
-        "-Subs $($Subs -join ',')"
+        "-Listing $Listing"
     ) -join ' '
 
     Register-ScheduledTask WallpaperCarousel `
         -Settings (New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -Priority 6 -RunOnlyIfNetworkAvailable) `
-        -Action (New-ScheduledTaskAction "pwsh.exe" -Argument $Arg -WorkingDirectory $PSScriptRoot) `
+        -Action (New-ScheduledTaskAction "pwsh.exe" -Argument $Arguments -WorkingDirectory $PSScriptRoot) `
         -Trigger (New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval $Interval) `
         -Force | Out-Null
 
     Write-Host "Added Task Schedule. Remove it by running `"Unregister-ScheduledTask WallpaperCarousel -Confirm:`$false`""
-    exit
 }
 
-$SubredditData = Invoke-WebRequest -Uri "https://www.reddit.com/r/$($Subs | Get-Random).json?listing=$Listing&t=$Timeframe&limit=$FetchLimit"
-$SubredditPosts = (ConvertFrom-Json $SubredditData.content).data.children | ForEach-Object { $_.data } | Where-Object {
-    -not $_.banned_by -and -not $_.removed_by -and -not $_.over_18 `
-        -and -not $_.is_video -and -not $_.spoiler -and $_.url `
-        -and $FileTypes.Contains($_.url.Split('.')[-1])
-}
-
-New-Item $WFolder -ItemType Directory -Force | Out-Null
-$Existing = Get-ChildItem $WFolder -Name
-
-foreach ($Post in $SubredditPosts) {
-    $Filename = Split-Path $Post.url -Leaf
-
-    if ($Existing -and $Existing.Contains($Filename)) {
-        Write-Debug "$WFolder\$filename already exists. Skipping..."
-        continue
+elseif ($Mode -eq 1) {
+    $SubredditData = Invoke-WebRequest -Uri "https://www.reddit.com/r/$($Subs | Get-Random).json?listing=$Listing&t=$Timeframe&limit=$Limit"
+    $SubredditPosts = (ConvertFrom-Json $SubredditData.content).data.children | ForEach-Object { $_.data } | Where-Object {
+        -not $_.banned_by -and -not $_.removed_by -and -not $_.over_18 `
+            -and -not $_.is_video -and -not $_.spoiler -and $_.url `
+            -and $FileTypes.Contains($_.url.Split('.')[-1])
     }
 
-    if ($Post.title -imatch $ResPattern -and -not (ValidateImage $Filename -H $Matches.Height -W $Matches.Width)) { continue }
+    New-Item $Dir -ItemType Directory -Force | Out-Null
+    $Existing = Get-ChildItem $Dir -Name
 
-    try { $Image = New-Object System.Drawing.Bitmap ((Invoke-WebRequest $Post.url).RawContentStream) }
-    catch { Write-Error "An error occurred while processing $filename from $($Post.url)"; continue }
+    foreach ($Post in $SubredditPosts) {
+        $Filename = Split-Path $Post.url -Leaf
 
-    if (-not (ValidateImage $Filename -H $Image.Height -W $Image.Width)) { continue }
+        if ($Existing -and $Existing.Contains($Filename)) {
+            Write-Debug "$Dir\$filename already exists. Skipping..."
+            continue
+        }
 
-    $Image.Save("$WFolder\$Filename")
-    Write-Debug "Saved $WFolder\$filename ($($Post.subreddit))"
+        if ($Post.title -imatch $ResPattern -and -not (ValidateImage $Filename -H $Matches.Height -W $Matches.Width)) { continue }
+
+        try { $Image = New-Object System.Drawing.Bitmap ((Invoke-WebRequest $Post.url).RawContentStream) }
+        catch { Write-Error "An error occurred while processing $filename from $($Post.url)"; continue }
+
+        if (-not (ValidateImage $Filename -H $Image.Height -W $Image.Width)) { continue }
+
+        $Image.Save("$Dir\$Filename")
+        Write-Debug "Saved $Dir\$filename ($($Post.subreddit))"
+    }
+}
+
+elseif ($Mode -eq 2) {
+    #TODO: Add sweeping
 }
